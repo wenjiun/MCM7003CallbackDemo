4badc08fd5e1640b7b24e013b170c7d88d84540e
Yap Wen Jiun <wenjiun@yahoo.com>
2023-08-21 20:35:07 +0800

    First deploy

---
+++ lecture10.py
@@ -0,0 +1,34 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+
+app = Dash(__name__)
+server = app.server
+
+app.title = "MCM7003 Data Visualization Interactive Demo" 
+
+fig1 = px.line(x=["a","b","c","d","e"], y=[1,3,2,2,3], title="Sample figure")	
+
+df = px.data.iris()
+fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
+
+app.layout = html.Div(
+    [html.H1("Data Visualization"),
+    dcc.RadioItems(id='my-radio', options=['Figure 1', 'Figure 2'], value='Figure 1', inline=True),
+    dcc.Graph(id='graph-output', figure ={})]
+)
+
+
+@app.callback(
+    Output(component_id='graph-output', component_property='figure'),
+    Input(component_id='my-radio', component_property='value')
+)
+
+def update_my_graph(val_chosen):
+    if (val_chosen == 'Figure 1'):
+        return fig1	
+    else: 
+	return fig2
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
